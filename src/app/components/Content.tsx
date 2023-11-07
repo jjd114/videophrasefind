b1ce4973929b6102b3da88aef7e34e3a46190fc1
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-11-07 13:14:03 +0300

    feat: load data from mock api

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,6 +1,10 @@
+"use client"
+import { useEventListener } from "../hooks/useEventListener";
+import { useEffect, useState } from "react";
+
 const ListElem = () => {
     return (
-        <div className="p-3 flex items-center">
+        <div className="p-2 flex items-center">
             <div className="w-[121px] h-[90px] rounded-xl bg-[#ffffff1f]">
             </div>
             <div className="ml-5">
@@ -15,15 +19,35 @@ const ListElem = () => {
     );
 }
 
-const Content = () => {
+const Content = ({ data }: any) => {
+    const onClick = () => {
+        alert('click');
+    }
     return (
         <div className="py-6 px-10 bg-[#212A36] h-full w-[100%] max-w-[627px]">
-            <div className="text-white text-xl font-semibold h-[48px]">
+            <div
+                className="text-white text-xl font-semibold h-[48px]"
+                id="button"
+            >
                 Results:  1000
             </div>
-            <div className="w-[100%] h-[379px] rounded-[20px] bg-[#ffffff1f] mt-3">
+            <div className="ml-3 w-[100%] h-[379px] rounded-[20px] bg-[#ffffff1f] mt-3">
+                <video style={{ width: "100%", height: "100%" }}>
+                    <source src={data.videoUrl} />
+                    <track
+                        label="English"
+                        kind="subtitles"
+                        srcLang="en"
+                        src={`data:text/vtt;charset=UTF-8,${encodeURIComponent(data.captionsVtt)}`}
+                        default
+                    />
+                </video>
             </div>
-            <div className="w-[100%] h-106 mt-5">
+            <div className="w-[100%] h-[420px] mt-5 overflow-auto">
+                <ListElem />
+                <ListElem />
+                <ListElem />
+                <ListElem />
                 <ListElem />
                 <ListElem />
                 <ListElem />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ import Image from 'next/image';
 import Sidebar from './components/Sidebar';
 import Content from './components/Content';
 
-export default function Home() {
+const request = async () => {
+  const res = await fetch('https://videophrasefind.vercel.app/mock-api/transcribe?videoUrl=some_video_url&query=some_query');
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await request();
   return (
-    <main className="bg-[#161E2A] h-238 flex justify-between">
+    <main className="bg-[#161E2A] h-[951px] flex justify-between">
       <Sidebar />
-      <Content />
+      <Content data={data} />
     </main>
   )
 }
