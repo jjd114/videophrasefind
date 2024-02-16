f85a104cf31b3704c5928c9cb29b40e5e8663cb9
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-02-16 19:00:13 +0300

    refactor: make contact, terms&cond, home pages a bit more responsive

--- src/app/terms-and-conditions/layout.tsx
+++ src/app/terms-and-conditions/layout.tsx
@@ -6,7 +6,7 @@ export default function TermsAndConditionsLayout({
   return (
     <section className="flex w-full flex-col">
       <div className="flex min-h-[430px] items-center justify-center bg-[#101824]">
-        <h1 className="text-4xl font-extrabold sm:text-5xl">
+        <h1 className="text-3xl font-extrabold sm:text-5xl">
           Terms and conditions
         </h1>
       </div>
