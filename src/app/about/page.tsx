103b2ca04ddd02053c12b83aadac4755bbacc945
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-02-19 17:22:19 +0300

    refactor: move home page info content to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -25,6 +25,41 @@ export default function About() {
           or let us know how we can improve here!
         </p>
       </div>
+      <section className="hide-scrollbar flex flex-1 flex-col gap-4 overflow-scroll rounded-[32px] bg-[#0B111A] p-8 md:p-12">
+        <div className="flex flex-1 flex-col gap-5 leading-7">
+          <h2 className="text-2xl font-semibold md:text-4xl">Video search</h2>
+          <div className="rounded-md bg-gradient-to-r from-purple-500/15 p-2">
+            <div className="p-5 text-white/85 md:text-lg">
+              <p>
+                Search a video for keyword for phrase by pasting a link or
+                uploading a movie file.
+              </p>
+            </div>
+          </div>
+          <h2 className="text-2xl font-semibold md:text-4xl">
+            Search a Video with Ease
+          </h2>
+          <div className="flex flex-1 flex-col rounded-md bg-gradient-to-r from-purple-500/15 p-2">
+            <div className="flex flex-1 flex-col justify-between gap-7 p-4 text-white/85 md:text-lg">
+              <p>
+                Our tool transcribes the video and allows a user to quickly sift
+                the results for any instance of a word or phrase. Jump to the
+                various instances of the word search quickly.
+              </p>
+              <p>
+                This may be helpful to search interviews or recorded meetings.
+                Scrubbing through hours of footage for a particular moment or
+                spoken word is time-consuming. This tool is dedicated to solving
+                that issue.
+              </p>
+              <p>
+                Other use cases can be for news reels, documentary footage, or
+                any video from YouTube that needs to be searched.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
   if (pathname === "/terms-and-conditions") return null;
 
   return (
-    <footer className="m-auto flex w-full max-w-screen-2xl justify-end bg-[#161E2A] px-7 py-10">
+    <footer className="m-auto flex w-full max-w-screen-2xl justify-end bg-[#161E2A] px-7 py-4">
       <div className="inline-flex flex-col">
-        <h2 className="my-3 text-sm font-medium">Company</h2>
+        <h2 className="mb-3 text-sm font-medium">Company</h2>
         <ul className="text-sm">
           <Link href="/terms-and-conditions">
             <li>Terms and conditions</li>
