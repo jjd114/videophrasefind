9abf444f200b9578a47968dffd17f25e213ca71d
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-02-21 13:55:20 +0300

    feat: highlight find word in header with purple color

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,7 +16,8 @@ const Header = () => {
     <header className="flex items-center justify-center bg-[#101824] px-7 py-4">
       <div className="flex w-full max-w-[calc(theme(screens.2xl)-2*theme(padding.7))] items-center justify-between max-[580px]:flex-col max-[580px]:gap-4">
         <Link href="/" className="text-2xl font-extrabold">
-          VideoPhraseFind
+          <span>VideoPhrase</span>
+          <span className="text-purple-600">Find</span>
         </Link>
         <nav>
           <ul className="flex gap-10">
