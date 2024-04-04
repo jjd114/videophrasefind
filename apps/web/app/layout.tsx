cf02f076b3a990e1e9a0c94f50d5750b605e2a5e
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-04-04 16:34:34 +0300

    feat: add ClerkProvider to root layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -28,19 +29,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} flex min-h-screen flex-col bg-[#161E2A]`}
-      >
-        <Analytics />
-        <QueryProvider>
-          <Header />
-          <main className="flex flex-1 bg-[#161E2A] px-3 py-7 sm:px-7">
-            {children}
-          </main>
-          <Footer />
-        </QueryProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className} flex min-h-screen flex-col bg-[#161E2A]`}
+        >
+          <Analytics />
+          <QueryProvider>
+            <Header />
+            <main className="flex flex-1 bg-[#161E2A] px-3 py-7 sm:px-7">
+              {children}
+            </main>
+            <Footer />
+          </QueryProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
