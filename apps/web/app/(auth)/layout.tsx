7b114e44d1c81e24a56ec43c5ff3f24c6987d1b4
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-04-04 17:02:43 +0300

    feat: add auth pages route group

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="flex w-full items-center justify-center px-3 py-7 sm:px-7">
+      {children}
+    </section>
+  );
+}
