f02a90095d3094de5d62816c365d2dad48397e0a
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-16 18:13:50 +0300

    refactor(web): change plans cards appearance a bit

--- apps/web/app/subscription/page.tsx
+++ apps/web/app/subscription/page.tsx
@@ -2,23 +2,35 @@ import { type Metadata } from "next";
 
 import { createCheckoutSession } from "@/app/stripe-actions";
 
+import Button from "@/components/Button";
+
 export const metadata: Metadata = {
   title: "Subscription",
 };
 
 export default function Contact() {
   return (
-    <section>
-      <div className="product">
-        <div className="description">
-          <h3>Pro plan</h3>
+    <section className="flex gap-8">
+      <div className="flex min-w-[300px] flex-col gap-12 rounded-xl bg-[#0B111A] p-8">
+        <div className="flex flex-col gap-6 text-center">
+          <h3 className="text-2xl font-bold">Pro plan</h3>
           <h5>$7.00 / month</h5>
         </div>
+        <form action={createCheckoutSession}>
+          <input type="hidden" name="lookup_key" value="pro-month" />
+          <Button>Checkout</Button>
+        </form>
       </div>
-      <form action={createCheckoutSession}>
-        <input type="hidden" name="lookup_key" value="pro-month" />
-        <button>Checkout</button>
-      </form>
+      <div className="flex min-w-[300px] flex-col gap-12 rounded-xl bg-[#0B111A] p-8">
+        <div className="flex flex-col gap-6 text-center">
+          <h3 className="text-2xl font-bold">Pro plan</h3>
+          <h5>$80.00 / month</h5>
+        </div>
+        <form action={createCheckoutSession}>
+          <input type="hidden" name="lookup_key" value="pro-year" />
+          <Button>Checkout</Button>
+        </form>
+      </div>
     </section>
   );
 }
