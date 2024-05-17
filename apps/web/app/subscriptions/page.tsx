863fa449a44ab9739d810d4929c03f3376b0d1ae
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-17 12:02:12 +0300

    refactor(web): rename route subscription to subscriptions

--- apps/web/app/subscriptions/page.tsx
+++ apps/web/app/subscriptions/page.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
         <div className="flex min-w-[300px] flex-col gap-12 rounded-xl bg-[#0B111A] p-8">
           <div className="flex flex-col gap-6 text-center">
             <h3 className="text-2xl font-bold">Pro plan</h3>
-            <h5>$80.00 / month</h5>
+            <h5>$80.00 / year</h5>
           </div>
           <form action={createCheckoutSession}>
             <input type="hidden" name="lookup_key" value="pro-year" />
