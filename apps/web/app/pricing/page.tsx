1bf700b8fdba9c54e0fc9235bdef36b313f6cbc8
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-17 18:40:49 +0300

    refactor(web): refactor pricing page

--- apps/web/app/pricing/page.tsx
+++ apps/web/app/pricing/page.tsx
@@ -1,5 +1,4 @@
 import { type Metadata } from "next";
-import { auth } from "@clerk/nextjs/server";
 
 import {
   createCheckoutSession,
@@ -12,34 +11,29 @@ export const metadata: Metadata = {
   title: "Subscription",
 };
 
-export default function Contact() {
-  const { userId } = auth();
+const types = ["month", "year"] as const;
 
+export default function Contact() {
   return (
     <section className="flex flex-col gap-6">
       <div className="flex gap-8">
-        <div className="flex min-w-[300px] flex-col gap-12 rounded-xl bg-[#0B111A] p-8">
-          <div className="flex flex-col gap-6 text-center">
-            <h3 className="text-2xl font-bold">Pro plan</h3>
-            <h5>$7.00 / month</h5>
+        {types.map((type) => (
+          <div
+            key={type}
+            className="flex min-w-[300px] flex-col gap-12 rounded-xl bg-[#0B111A] p-8"
+          >
+            <div className="flex flex-col gap-6 text-center">
+              <h3 className="text-2xl font-bold">Pro plan</h3>
+              <h5>{`$${type === "month" ? "7" : "80"}.00 / ${type}`}</h5>
+            </div>
+            <form action={createCheckoutSession}>
+              <input type="hidden" name="lookup_key" value={`pro-${type}`} />
+              <Button>Checkout</Button>
+            </form>
           </div>
-          <form action={createCheckoutSession}>
-            <input type="hidden" name="lookup_key" value="pro-month" />
-            <Button>Checkout</Button>
-          </form>
-        </div>
-        <div className="flex min-w-[300px] flex-col gap-12 rounded-xl bg-[#0B111A] p-8">
-          <div className="flex flex-col gap-6 text-center">
-            <h3 className="text-2xl font-bold">Pro plan</h3>
-            <h5>$80.00 / year</h5>
-          </div>
-          <form action={createCheckoutSession}>
-            <input type="hidden" name="lookup_key" value="pro-year" />
-            <Button>Checkout</Button>
-          </form>
-        </div>
+        ))}
       </div>
-      {userId && (
+      {true && (
         <form className="text-center font-bold" action={createPortalSession}>
           <button id="checkout-and-portal-button" type="submit">
             <span className="underline">Manage your billing information</span>
