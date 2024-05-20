b03825a180c1c90857adc1cf57739bbbd019d4a5
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-20 16:19:57 +0300

    feat: add logic to events we're listening to

--- apps/web/app/api/webhooks/stripe/route.ts
+++ apps/web/app/api/webhooks/stripe/route.ts
@@ -21,27 +21,57 @@ export async function POST(req: Request) {
     return new Response("Webhook Error!");
   }
 
+  console.log(`event: ${event.type}`);
+
   // - save userId, stripeCustomerId, stripeSubscriptionId, membership status in the database
   // - add transaction in the Transaction table
   // - event.data.object is a checkout session (https://docs.stripe.com/api/events/types#checkout_session_object)
   if (event.type === "checkout.session.completed") {
-    console.log({ eventType: event.type });
+    console.log(`handling: ${event.type}`);
+
+    const membership = await db.membership.findUnique({
+      where: { userId: event.data.object.client_reference_id as string },
+    });
 
     const subscription = await stripe.subscriptions.retrieve(
       event.data.object.subscription as string,
     );
 
-    // await db.membership.update({
-    //   where: {
-    //     stripeSubscriptionId: subscription.id,
-    //   },
-    //   data: {
-    //     status: "active",
-    //     userId: event.data.object.client_reference_id as string,
-    //     stripeCustomerId: subscription.customer as string,
-    //     stripeSubscriptionId: subscription.id,
-    //   },
-    // });
+    if (!membership?.userId) {
+      await db.membership.create({
+        data: {
+          status: "active",
+          userId: event.data.object.client_reference_id as string,
+          stripeCustomerId: subscription.customer as string,
+          stripeSubscriptionId: subscription.id,
+          stripeCurrentPeriodEnd: new Date(
+            subscription.current_period_end * 1000,
+          ),
+        },
+      });
+    } else {
+      await db.membership.update({
+        where: { userId: membership.userId },
+        data: {
+          status: "active",
+          stripeCustomerId: subscription.customer as string,
+          stripeSubscriptionId: subscription.id,
+          stripeCurrentPeriodEnd: new Date(
+            subscription.current_period_end * 1000,
+          ),
+        },
+      });
+    }
+
+    const subscriptionInterval = subscription.items.data[0].plan.interval;
+
+    await db.transaction.create({
+      data: {
+        credits: subscriptionInterval === "month" ? 3000 : 3000 * 12,
+        description: "Subscribed",
+        userId: event.data.object.client_reference_id as string,
+      },
+    });
   }
 
   // Continue to provision the subscription as payments continue to be made.
@@ -50,40 +80,85 @@ export async function POST(req: Request) {
   // - add transaction in the Transaction table
   // - event.data.object is an invoice (https://docs.stripe.com/api/invoices/object)
   if (event.type === "invoice.paid") {
-    console.log({ eventType: event.type });
+    console.log(`handling: ${event.type}`);
+
+    const result = await db.membership.findUnique({
+      where: {
+        stripeSubscriptionId: event.data.object.subscription as string,
+      },
+    });
+
+    if (!result) {
+      console.log(
+        "We need to wait for the initial subscription before provision",
+      );
+      return new Response(null, { status: 200 });
+    }
 
     const subscription = await stripe.subscriptions.retrieve(
       event.data.object.subscription as string,
     );
 
-    // await db.membership.create({
-    //   data: {
-    //     stripeCurrentPeriodEnd: new Date(
-    //       subscription.current_period_end
-    //     ),
-    //   },
-    // });
+    console.log({ result });
+
+    const { userId } = await db.membership.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        status: "active",
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000,
+        ),
+      },
+    });
+
+    const subscriptionInterval = subscription.items.data[0].plan.interval;
+
+    await db.transaction.create({
+      data: {
+        userId,
+        credits: subscriptionInterval === "month" ? 3000 : 3000 * 12,
+        description: "Continue to provision the subscription",
+      },
+    });
   }
 
   // Occurs whenever an invoice payment attempt fails,
   // due either to a declined payment or to the lack of a stored payment method.
   // - event.data.object is an invoice (https://docs.stripe.com/api/invoices/object)
   if (event.type === "invoice.payment_failed") {
-    console.log({ eventType: event.type });
+    console.log(`handling: ${event.type}`);
 
-    const subscription = await stripe.subscriptions.retrieve(
-      event.data.object.subscription as string,
-    );
+    await db.membership.update({
+      where: {
+        stripeSubscriptionId: event.data.object.subscription as string,
+      },
+      data: {
+        status: "paused",
+        stripeCurrentPeriodEnd: null,
+      },
+    });
+  }
 
-    // await db.membership.update({
-    //   where: {
-    //     stripeSubscriptionId: subscription.id,
-    //   },
-    //   data: {
-    //     status: "inactive",
-    //     stripeCurrentPeriodEnd: null,
-    //   },
-    // });
+  if (event.type === "customer.subscription.deleted") {
+    try {
+      console.log(`handling: ${event.type}`);
+
+      await db.membership.update({
+        where: {
+          stripeSubscriptionId: event.data.object.id,
+        },
+        data: {
+          status: "inactive",
+          stripeCustomerId: null,
+          stripeSubscriptionId: null,
+          stripeCurrentPeriodEnd: null,
+        },
+      });
+    } catch (err) {
+      console.log("Subscription not found in the database", err);
+    }
   }
 
   return new Response(null, { status: 200 });
