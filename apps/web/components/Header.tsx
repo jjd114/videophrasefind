1478486af124364f402f0f1549a0151be5d4b472
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-05-21 12:58:26 +0300

    refactor(web): refactor Header

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -14,13 +14,13 @@ import { Icons } from "@/components/Icons";
 
 import { cn } from "@/lib/utils";
 
-const defaultTabs = ["/", "/about", "/help", "/contact", "/pricing"] as const;
+const leftTabs = ["/", "/about", "/help", "/contact", "/pricing"] as const;
 
 const capitalize = (str: string) => {
   return str[0].toUpperCase() + str.slice(1);
 };
 
-const tabText = (tab: (typeof defaultTabs)[number]) => {
+const tabText = (tab: (typeof leftTabs)[number]) => {
   switch (tab) {
     case "/":
       return "Home";
@@ -34,68 +34,73 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-center bg-[#101824] px-7 py-4">
-      <div className="flex w-full max-w-[calc(theme(screens.2xl)-2*theme(padding.7))] items-center justify-between max-[580px]:flex-col max-[580px]:gap-4">
-        <Link href="/" className="text-2xl font-extrabold">
+      <div className="flex w-full max-w-[calc(theme(screens.2xl)-2*theme(padding.7))] items-center gap-8 max-[580px]:flex-col max-[580px]:gap-4">
+        <Link href="/" className="text-xl font-extrabold">
           <span>VideoPhrase</span>
           <span className="text-purple-600">Find</span>
         </Link>
-        <nav className="flex items-center gap-8">
-          <SignedIn>
-            <Link
-              href="/videos"
-              className={cn(
-                "min-w-[63px] text-center font-medium transition-colors hover:text-neutral-300",
-                {
-                  "font-bold": pathname === "/videos",
-                },
-              )}
-            >
-              Videos
-            </Link>
-          </SignedIn>
-          <ul className="flex gap-8">
-            {defaultTabs.map((tab) => (
-              <Link
-                key={tab}
-                href={tab}
-                className={cn(
-                  "font-medium transition-colors hover:text-neutral-300",
-                  {
-                    "font-bold": tab === pathname,
-                  },
-                )}
-              >
-                <li className="min-w-[63px] text-center">{tabText(tab)}</li>
-              </Link>
-            ))}
-          </ul>
-          <div className="flex w-[63px] justify-center">
-            <ClerkLoading>
-              <Icons.spinner className="size-6 animate-spin text-[#9DA3AE]" />
-            </ClerkLoading>
-            <ClerkLoaded>
-              <SignedOut>
+        <div className="flex flex-1 items-center justify-between">
+          <nav className="flex items-center">
+            <ul className="flex gap-8">
+              {leftTabs.map((tab) => (
                 <Link
-                  href="/sign-in"
+                  key={tab}
+                  href={tab}
                   className={cn(
-                    "text-center font-medium transition-colors hover:text-neutral-300",
+                    "text-sm font-medium transition-colors hover:text-neutral-300",
                     {
-                      "font-bold":
-                        pathname ===
-                          process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL ||
-                        pathname === process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+                      "font-bold": tab === pathname,
                     },
                   )}
                 >
-                  Sign in
+                  <li className="min-w-[63px] text-center">{tabText(tab)}</li>
                 </Link>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </ClerkLoaded>
-          </div>
-        </nav>
+              ))}
+            </ul>
+          </nav>
+          <nav className="flex items-center gap-8">
+            <SignedIn>
+              <Link
+                href="/videos"
+                className={cn(
+                  "min-w-[63px] text-center text-sm font-medium transition-colors hover:text-neutral-300",
+                  {
+                    "font-bold": pathname === "/videos",
+                  },
+                )}
+              >
+                Videos
+              </Link>
+            </SignedIn>
+            <div className="flex w-[63px] justify-center">
+              <ClerkLoading>
+                <Icons.spinner className="size-5 animate-spin text-[#9DA3AE]" />
+              </ClerkLoading>
+              <ClerkLoaded>
+                <SignedOut>
+                  <Link
+                    href="/sign-in"
+                    className={cn(
+                      "text-center text-sm font-medium transition-colors hover:text-neutral-300",
+                      {
+                        "font-bold":
+                          pathname ===
+                            process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL ||
+                          pathname ===
+                            process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+                      },
+                    )}
+                  >
+                    Sign in
+                  </Link>
+                </SignedOut>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </ClerkLoaded>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   );
