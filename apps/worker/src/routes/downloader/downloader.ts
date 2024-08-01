5ed805848362370e8da6f525b2c86eb0005ce63d
eug-vs <eugene@eug-vs.xyz>
2024-08-01 19:37:33 +0200

    feat(worker): use mp4 muxer on mkv stream

--- apps/worker/src/routes/downloader/downloader.ts
+++ apps/worker/src/routes/downloader/downloader.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import YTDlpWrap from "yt-dlp-wrap";
 import { db } from "database";
+import ffmpeg from "fluent-ffmpeg";
+import { spawn } from "node:child_process";
 
 const ytDlpWrap = new YTDlpWrap(process.env.YT_DLP_PATH || "/usr/bin/yt-dlp");
 
@@ -24,7 +26,7 @@ async function getVideoAndAudioStreamID(url: string) {
   const filteredFormats = formats
     .split("\n")
     .filter((s) =>
-      s.match(/((https.*720p|https.*480p)|(audio only.*medium.*))/)
+      s.match(/((https.*720p|https.*480p)|(audio only.*medium.*))/),
     );
   console.log(filteredFormats);
 
@@ -33,7 +35,7 @@ async function getVideoAndAudioStreamID(url: string) {
       ({
         type: s.includes("audio only") ? "audio" : "video",
         id: s.split(" ")[0],
-      }) as const
+      }) as const,
   );
 }
 
@@ -66,14 +68,30 @@ app.post("/fetch-and-trigger", async (c) => {
 
   const { upload, passThrough } = streamToS3(videoId, "full");
 
-  ytDlpWrap
-    .execStream([
-      "-f",
-      `${audioStream?.id || "ba"}+${videoStream?.id || "bv"}`,
-      '--merge-output-format',
-      'mp4',
-      url,
-    ])
+  // Example command for testing
+  // yt-dlp "https://www.youtube.com/watch?v=8GhFmQPZAlo" -f bestvideo+bestaudio --merge-output-format mkv -o - |
+  // ffmpeg -i pipe:0 -movflags frag_keyframe+empty_moov  -c copy -f mp4 pipe:1 |
+  // mpv -
+
+  // Not using yt-dlp-wrap to narrow down error surface here, probably shoud just get rid of extra dependency
+  const ytDlpStream = spawn(process.env.YT_DLP_PATH || "/usr/bin/yt-dlp", [
+    url,
+    "-f",
+    `${audioStream?.id || "ba"}+${videoStream?.id || "bv"}`,
+    "--merge-output-format",
+    "mkv", // MKV (matroska) stream is the only one that reliably (?) contains video & audio
+    "-o",
+    "-",
+  ]);
+
+  ytDlpStream.stderr.on("data", (data: Buffer) =>
+    console.warn(data.toString()),
+  );
+
+  // MKV is not supported by browsers (and it's also a data stream that is missing headers and metadata),
+  // so we use mp4 muxer to convert it to a full-ass mp4 file
+  ffmpeg(ytDlpStream.stdout)
+    .addOptions(["-movflags frag_keyframe+empty_moov", "-c copy", "-f mp4"])
     .pipe(passThrough);
 
   await upload.done();
