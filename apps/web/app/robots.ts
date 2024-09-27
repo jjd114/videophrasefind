22ffe7f063075f74f1bc7768e7426c69af33fac3
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-09-27 13:55:38 +0300

    refactor(robots): change disallow BASE_URL check to siftvid.io

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -4,8 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      disallow:
-        process.env.BASE_URL !== "https://videophrasefind.com" ? "/" : [""],
+      disallow: process.env.BASE_URL !== "https://www.siftvid.io" ? "/" : [""],
     },
     sitemap: `${process.env.BASE_URL}/sitemap.xml`,
   };
